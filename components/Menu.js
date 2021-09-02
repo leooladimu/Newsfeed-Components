097dfc7cb09b2a8eb6f0499070a54a2cc94f25b5
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-09-02 01:12:25 -0400

    four

--- components/Menu.js
+++ components/Menu.js
@@ -18,27 +18,27 @@ let menuItems = [
     </ul>
   </div>
 
-  The 'menuMaker' takes an array of menu items as its only argument.
+//   The 'menuMaker' takes an array of menu items as its only argument.
 
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
-  Add those items to the <ul>
+//   Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+//   Add those items to the <ul>
 
-  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+//   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
 
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+//   Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
 
-  Step 5: Don't forget to return your div.menu.
+//   Step 5: Don't forget to return your div.menu.
 
-  Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
-*/
-function menuMaker([
-  
-])
+//   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
+// */
 
-{/* <div class="menu">
-<ul>
-  {each menu item as an <li>}
-</ul>
-</div>
-
-The 'menuMaker' takes an array of menu items as its only argument. */}
+function menuMaker(menuItems) {
+  const menu = document.createElement('div');
+  const unli = document.createElement('ul');
+  const students = document.createElement('li');
+  const faculty = document.createElement('li');
+  const whatsNew = document.createElement('li');
+  const techTrends = document.createElement('li');
+  const music = document.createElement('li');
+  const logOut = document.createElement('li');
+}
